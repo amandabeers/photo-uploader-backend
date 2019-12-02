c691e86fe489c15a6bb8c527aec3c79446d8d64a
amandabeers <amanda.green413@gmail.com>
2019-12-02 16:24:10 -0500

    Set up AWS S3 uploads and posts routes
    -added functions to handle uploads and deletions from S3
    -added index, show, create, update, and delete routs for posts resource

--- app/models/post.js
+++ app/models/post.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-  description: {
+  name: {
     type: String,
     required: true
+  },
+  description: {
+    type: String
   },
   type: {
     type: String,
@@ -11,6 +14,11 @@ const postSchema = new mongoose.Schema({
   },
   url: {
     type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   }
 }, {
